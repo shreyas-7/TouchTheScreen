5887df60e0d9fea2cbcfe127c8428f644683ac32
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2017-05-30 10:45:47 +0530

    Update ml_function.py

--- final/ml_function.py
+++ final/ml_function.py
@@ -2,17 +2,21 @@ import ml
 import pandas as pd 
 from sklearn.svm import SVC
 
+
 svcmodel_x = SVR()
 svcmodel_y = SVR()
 
-x_df_ans = np.array(x_df.x_pos)
-y_df_ans = np.array(y_df.y_pos)
+x_df_app = np.array(x_df.app_x)
+y_df_app = np.array(y_df.app_y)
 
-x_df_val = np.array(x_df.x_values)
-y_df.val = np.array(y_df.y_values)
+x_df_acc = np.array(x_df.acc_x)
+y_df_acc = np.array(y_df.acc_y)
 
-svcmodel_x.fit(x_df_val,x_df_ans)
-svcmodel_y.fit(y_df_val,y_df_ans)
+x_df_val = np.array(x_df.x_val)
+y_df_val = np.array(y_df.y_val)
+
+svcmodel_x.fit(np.array([x_df_app,x_df_val]),x_df_ans)
+svcmodel_y.fit(np.array([y_df_app,y_df_val]),y_df_ans)
 
 def prediction_of(x,y) :
 	return svcmodel_x.predict(x) , svcmodel_y.predict(y)

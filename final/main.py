315bfc780ada08b3fe8fc8ef6a55d9448df3f938
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2017-05-31 13:56:39 +0530

    Update main.py

--- final/main.py
+++ final/main.py
@@ -1,24 +1,18 @@
 import numpy as np
-
-import pandas as pd
-
+import pandas as pd 
+import matplotlib.pyplot as plt
 import time
-
 from pynput.mouse import Button, Controller
 mouse = Controller()
-
 import pyautogui
 
-from sklearn.svm import SVR
-
-import functions
-
-import ml
+import functions.py 
+import ml.py
 
 # assuming I have created a function named predictions_x(x) , predictions_y(y)
 # memory array will be like
 """
-if 0 fingers - []
+if 0 fingers - [[]]
 if 1 finger - [position]
 if 2 fingers - [position,position]]
 """
@@ -32,6 +26,7 @@ no_of_prev_measurements = 10
 probablity = 7
 error1 = 10
 epsilon = 10
+drag_variable = 0
 
 for i in range(iniitial_times) :
 	collection_without_normalisation()
@@ -56,9 +51,6 @@ def main () :
 
 		no_of_fingers, x_pixels, y_pixels = process_values(curr_values[0],curr_values[1])
 
-		#if no_of_fingers == "Error" :
-		#	continue
-
 		#approximate_pixel_array
 		try :
 			for i in range(np.size(x_pixels)) :
@@ -73,36 +65,29 @@ def main () :
 		if no_of_fingers == 0 :
 			memory_array_x[i] = []
 			memory_array_y[i] = []
+			drag_variable = 0
+			mouse.release(Button.left)
 
 		elif no_of_fingers == 1 :
 			memory_array_x[i] = x_pixels
 			memory_array_y[i] = y_pixels
 			mouse.position(approximate_pixels[0],approximate_pixels[1])
-			#######execute only one among these
 			do_click(i)
 			do_doubleclick(i)
 			do_rightclick(i)
-			#do_drag(i)
+			do_drag(i)
 
 
 		elif no_of_fingers == 2 :
-
-		#	what_is_the_scene()
-		#	mouse.position(approximate_pixels[0],approximate_pixels[1])
-		#	memory_array_x[i] = x_pixelsi
-		#	memory_array_y[i] = y_pixels
-		#	scroll()
-		#	pinch_zoom()
-
 			key0 = find_key(i,0)
 			key1 = find_key(i,1)
 
 			if key0 == key1 :
 				do_scroll(key)
 
-			if distance_between_fingers(i) > distance_between_fingers(i-1) :
+			if distance_between_fingers(i) > distance_between_fingers(i-1) + error1:
 				do_pinch_zoom('out')
-			elif distance_between_fingers(i) < distance_between_fingers(i-1) :
+			elif distance_between_fingers(i) < distance_between_fingers(i-1) + error1:
 				do_pinch_zoom('in')
 
 

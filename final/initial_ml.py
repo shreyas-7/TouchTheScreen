bdb22f11c67270b3b5443999d3dd40ea05e9f726
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2017-06-01 12:14:42 +0530

    Update initial_ml.py

--- final/initial_ml.py
+++ final/initial_ml.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-x_columns = np.array(['app_x','val_x','acc_x'])
-y_columns = np.array(['app_y','val_y','acc_y'])
+x_columns = np.array(['app_x','xm2','xm1','x','xp1','xp2','acc_x'])
+y_columns = np.array(['app_y','xm2','xm1','x','xp1','xp2','acc_y'])
 
-x_df = pd.DataFrame({}, index = None, columns = x_columns)
-y_df = pd.DataFrame({}, index = None, columns = y_columns)
+x_df = pd.DataFrame({}, columns = x_columns)
+y_df = pd.DataFrame({}, columns = y_columns)
 
 x_df.to_csv('x_data.csv')
 y_df.to_csv('y_data.csv')
